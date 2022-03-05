c7b295323fae3f281204424b1d4022beb3fff686
Gemechisgk <55735419+Gemechisgk@users.noreply.github.com>
2022-03-05 00:39:37 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+    $('.accordion-heading').click(function(){
+
+        $('.accordion .accordion-content').slideUp();
+
+        $(this).next('.accordion-content').slideDown();
+
+    });
+
+});
+var modal = document.getElementById('login-form');
+window.onclick = function(event) 
+{
+    if (event.target == modal) 
+    {
+        modal.style.display = "none";
+    }
+}
